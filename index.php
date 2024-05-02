a2ca2d02316a8e248b01b2fb538e60222bf3228f
JUSTIN TUBAÑA <163080250+BossCebbbbb@users.noreply.github.com>
2024-05-02 22:29:31 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include('includes/header.php');?>
+    
+<div class="py-5">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>POS PHP</h1>
+                
+                <a href="Login.php" class="btn btn-primary mt-4">Log in</a>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+   
+    <?php include('includes/footer.php');?>
